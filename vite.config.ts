a3ba1479da5b44bdbd9fd6c895825117060f4429
Sebastian <sebastin.soecker@gmail.come>
2025-10-21 15:05:37 -0600

    2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/soduko/' : '/',
   plugins: [
     react(),
     VitePWA({
@@ -39,4 +40,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
